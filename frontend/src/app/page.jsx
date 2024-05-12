ca6649f21d02055479ebe94c968b96afe5fb1647
m-triple-m <triplem656@gmail.com>
2024-05-12 12:56:53 +0530

    ..

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,47 +1,24 @@
-import { Container, Text, Button, Group } from '@mantine/core';
-// import { GithubIcon } from '@mantinex/dev-icons';
-import classes from './page.module.css';
-import { Icon3dRotate } from '@tabler/icons-react';
 
-export default function Home() {
-  return (
-    <div className={classes.wrapper}>
-      <Container size={'xl'} className={classes.inner}>
-        <h1 className={classes.title}>
-          A{' '}
-          <Text component="span" variant="gradient" gradient={{ from: 'blue', to: 'cyan' }} inherit>
-            fully featured
-          </Text>{' '}
-          React components and hooks library
-        </h1>
+import React from 'react'
+import { HeroBullets } from './home/HeroBullets'
+import { FeaturesCards } from './home/FeaturesCards'
+import { EmailBanner } from './home/EmailBanner'
+import { StatsGroup } from './home/StatsGroup'
+import { CardsCarousel } from './home/CardsCarousel'
+import { CarouselCard } from './home/CarouselCard'
+import '@mantine/carousel/styles.css';
 
-        <Text className={classes.description} color="dimmed">
-          Build fully functional accessible web applications with ease – Mantine includes more than
-          100 customizable components and hooks to cover you in any situation
-        </Text>
-
-        <Group className={classes.controls}>
-          <Button
-            size="xl"
-            className={classes.control}
-            variant="gradient"
-            gradient={{ from: 'blue', to: 'cyan' }}
-          >
-            Get started
-          </Button>
-
-          <Button
-            component="a"
-            href="https://github.com/mantinedev/mantine"
-            size="xl"
-            variant="default"
-            className={classes.control}
-            leftSection={<Icon3dRotate size={20} />}
-          >
-            GitHub
-          </Button>
-        </Group>
-      </Container>
-    </div>
-  );
+const Home = () => {
+    return (
+        <div>
+            <HeroBullets></HeroBullets>
+            <CardsCarousel></CardsCarousel>
+            <CarouselCard></CarouselCard>
+            <FeaturesCards></FeaturesCards>
+            <EmailBanner></EmailBanner>
+            <StatsGroup></StatsGroup>
+        </div>
+    )
 }
+
+export default Home
