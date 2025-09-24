b2844e4015eb7afbb7443c0f0ab65faba5009158
Ashish7794 <ashish7794ay@gmail.com>
2025-09-24 08:59:57 +0530

    fix

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const url = "mongodb+srv://avijeet7505:anish@cluster0.hwv9idx.mongodb.net/sitemap?retryWrites=true&w=majority&appName=Cluster0"
+const url = "mongodb+srv://avijeet7505:anish@cluster0.hwv9idx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 
 
 //asynchronous function
